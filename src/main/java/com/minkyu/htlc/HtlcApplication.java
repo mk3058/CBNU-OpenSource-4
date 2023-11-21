e900d5ae4c73301d91c87f9b8ddf51400f5307ae
mk3058 <kmk3058@icloud.com>
2023-11-21 15:19:04 +0900

    initial commit

---
+++ src/main/java/com/minkyu/htlc/HtlcApplication.java
@@ -0,0 +1,13 @@
+package com.minkyu.htlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtlcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtlcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minkyu/htlc/HtlcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minkyu.htlc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtlcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
