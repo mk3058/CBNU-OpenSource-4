d2f194600af72bb2f6f5632ac49a1dc287398b49
mk3058 <kmk3058@icloud.com>
2023-11-27 21:38:55 +0900

    feat: add frontend logic

---
+++ eth/src/ethereumFront.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import EthereumBackend from './ethereumBack';
+
+const WalletComponent = () => {
+    const [balance, setBalance] = useState(0);
+    const [receiver, setReceiver] = useState('');
+    const [amount, setAmount] = useState('');
+    const [transactionStatus, setTransactionStatus] = useState('');
+
+    useEffect(() => {
+        const loadBalance = async () => {
+            const balance = await EthereumBackend.getBalance();
+            setBalance(balance);
+        };
+
+        loadBalance();
+    }, []);
+
+    const handleSend = async () => {
+        try {
+            setTransactionStatus('전송 중...');
+            await EthereumBackend.sendEther(receiver, amount);
+            setTransactionStatus('전송 완료!');
+            setReceiver('');
+            setAmount('');           
+            const updatedBalance = await EthereumBackend.getBalance();
+            setBalance(updatedBalance);
+        } catch (error) {
+            setTransactionStatus('전송 실패: ' + error.message);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Wallet Info</h2>
+            <p>잔액: {balance.toString()} ETH</p>
+            <input 
+                type="text"
+                value={receiver}
+                onChange={(e) => setReceiver(e.target.value)}
+                placeholder="받는 사람 주소"
+            />
+            <input 
+                type="text"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="보낼 이더량"
+            />
+            <button onClick={handleSend}>송금</button>
+            <p>{transactionStatus}</p>
+        </div>
+        
+    );
+};
+
+export default WalletComponent;
